11cda80c9f271337a81ddf22ecc496f0b230326f
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-04-28 17:37:17 +0300

    Add sum between two integers function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(num_1, num_2) {
+    let result=0;
+    if (num_1 < 0 || num_2< 0 || !Number.isInteger(num_1)|| !Number.isInteger(num_1)|| typeof(num_1) ==="string"||typeof(num_2) ==="string"||typeof(num_1) ==="object"||typeof(num_2) ==="object") {
+        return 'ERROR';
+    }
+    else{
+        if(num_1>num_2){
+            for(let i = num_2; i<=num_1; i++){
+                result = result + i;   
+            
+            }
+        }
+        else{
+            for(let i = num_1; i<=num_2; i++){
+                result = result + i;    
+            }
+    
+        }
+        
+    };
+    return result;
 };
-
 // Do not edit below this line
 module.exports = sumAll;

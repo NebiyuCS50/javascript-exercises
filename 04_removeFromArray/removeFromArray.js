67294e28274d78c744b9bf3d4bfc7fc463cdcbd5
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-04-28 17:09:01 +0300

    Remove specified values from array and return filtered result

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...num) {
+    const result = array.filter(item =>!num.includes(item));
+    return result;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;

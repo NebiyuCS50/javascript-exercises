bdc877b84e9bad35be5986c751dab1b03400ff3f
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-04-28 15:32:49 +0300

    Add function to loop a string multiple times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(String, num) {
+    let str = "";
+    if (num<0) {
+        return "ERROR";
+    }
+    else{
+    for (i=1; i<=num; i++) {
+         str = str + String
+    }
+    }
+    return str;
 };
 
 // Do not edit below this line

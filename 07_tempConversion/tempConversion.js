fd557334e76faf04f18dd5e2073b8c3df4c3c7bc
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-05-09 12:55:23 +0300

    feat: tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(deg) {
+  const result = (deg-32)*5/9;
+  const roundResult = Math.round(result*10)/10;
+  return roundResult;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(deg) {
+  const result = (deg*9/5+32);
+  const roundResult = Math.round(result*10)/10;
+  return roundResult;
 };
 
 // Do not edit below this line

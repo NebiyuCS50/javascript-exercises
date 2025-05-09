01b31172f6ad9ac90c3c6b22f9a6318c98ec924a
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-05-09 14:29:51 +0300

    Done fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num <0){
+        return 'OOPS'
+    }
+    let sum =0;
+    let array = [0,1];
+    for(i = 2; i<=50;i++){
+        array.push(array[i-2]+ array[i-1]);
+    }
+    return array[num];
 };
 
 // Do not edit below this line

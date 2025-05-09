386f149fe5836b67fa4cec8ed33055d86d8fae75
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-05-09 14:03:52 +0300

    Done palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (palindrome) {
+    const cleaned = palindrome.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const reversed = cleaned.split('').reverse().join('');
+    return cleaned === reversed;
 };
 
 // Do not edit below this line

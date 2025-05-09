131a3dbbbb1421b57c56133dc853c8695d810f62
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-05-09 13:44:01 +0300

    create calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const total = array.reduce((pre,next)=>{
+    return pre+next;
+  },0)
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const total = array.reduce((pre,nxt)=>{
+    return pre*nxt;
+  })
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = 1;
+  if(a===0){
+    return 1;
+  }
+	for(i = 1; i<=a; i++){
+    total = total *i;
+  }
+  return total;
 };
 
 // Do not edit below this line
